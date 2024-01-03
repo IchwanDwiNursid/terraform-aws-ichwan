08d6b9156ca92fda9d5e99eb75e42ac26dbb3cb0
Thikpad t480 <idwinursid@gmail.com>
2024-01-03 09:50:34 +0700

    first commit

---
+++ ec2/variables.tf
@@ -0,0 +1,25 @@
+variable "aws_access_key" {
+  type    = string
+  # default = "AKIA2P7PSBJK7KLMN7NL"
+}
+
+variable "aws_secret_key" {
+  type    = string
+  # default = "4tAv0FoMvy5scp19P3Xos63OE2JxRS3bxjc2cZm8"
+}
+
+// ==== EC2 ====
+variable "list_vm" {
+  type    = list(string)
+  default = ["vm1", "vm2"]
+}
+
+variable "security_group_names" {
+  type = list(string)
+}
+
+// ==== VPC ====
+# variable "vpc_id" {
+#   type    = string
+#   default = "vpc-0a7518d1f364ea5da" // ID VPC
+# }
